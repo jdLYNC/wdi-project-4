7efe3f2e0d466b6d8172242c19d28277b24d7a5c
jdLYNC <joshlynch@live.co.uk>
2017-10-25 17:08:49 +0100

    refactored backend center secure route to factory function

--- config/routes.js
+++ config/routes.js
@@ -3,16 +3,16 @@ const jobs = require('../controllers/jobs');
 const certifications = require('../controllers/certifications');
 const messages = require('../controllers/messages');
 const auth  = require('../controllers/auth');
-const { userRoute, centerRoute } = require('../lib/secureRoute');
+const userRoute = require('../lib/secureRoute');
 
 router.route('/jobs')
-  .get( jobs.index)
-  .post(centerRoute, jobs.create);
+  .get(jobs.index)
+  .post(userRoute(false), jobs.create);
 
 router.route('/jobs/:id')
   .get(jobs.show)
-  .put(centerRoute, jobs.update)
-  .delete(centerRoute, jobs.delete);
+  .put(userRoute(false), jobs.update)
+  .delete(userRoute(false), jobs.delete);
 
 router.route('/certifications')
   .get(certifications.index);
@@ -21,11 +21,11 @@ router.route('/certifications/:id')
   .get(certifications.show);
 
 router.route('/messages')
-  .get(userRoute, messages.index)
-  .post(userRoute, messages.create);
+  .get(userRoute(true), messages.index)
+  .post(userRoute(true), messages.create);
 
 router.route('/messages/:id')
-  .delete(userRoute, messages.delete);
+  .delete(userRoute(true), messages.delete);
 
 router.route('/register')
   .post(auth.register);
