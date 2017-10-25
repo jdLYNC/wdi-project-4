f2d176ce60d928aeb2fac2cd3f3341b5a57f7431
jdLYNC <joshlynch@live.co.uk>
2017-10-25 16:22:28 +0100

    added center backend secure route

--- config/routes.js
+++ config/routes.js
@@ -3,16 +3,16 @@ const jobs = require('../controllers/jobs');
 const certifications = require('../controllers/certifications');
 const messages = require('../controllers/messages');
 const auth  = require('../controllers/auth');
-const secureRoute = require('../lib/secureRoute');
+const { userRoute, centerRoute } = require('../lib/secureRoute');
 
 router.route('/jobs')
-  .get(jobs.index)
-  .post(jobs.create);
+  .get( jobs.index)
+  .post(centerRoute, jobs.create);
 
 router.route('/jobs/:id')
   .get(jobs.show)
-  .put(jobs.update)
-  .delete(jobs.delete);
+  .put(centerRoute, jobs.update)
+  .delete(centerRoute, jobs.delete);
 
 router.route('/certifications')
   .get(certifications.index);
@@ -21,11 +21,11 @@ router.route('/certifications/:id')
   .get(certifications.show);
 
 router.route('/messages')
-  .get(messages.index)
-  .post(messages.create);
+  .get(userRoute, messages.index)
+  .post(userRoute, messages.create);
 
 router.route('/messages/:id')
-  .delete(messages.delete);
+  .delete(userRoute, messages.delete);
 
 router.route('/register')
   .post(auth.register);
